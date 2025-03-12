511bb792fe2aada917ce1c6cfc9b68ec08a5d4ed
Jeremy <68441282+Jampamane@users.noreply.github.com>
2025-03-12 02:53:56 +0000

    changed reference to actual background

--- docs/javascripts/smooth_homepage_image.js
+++ docs/javascripts/smooth_homepage_image.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bgContainer = document.getElementById("homepage-hero");
-    const imageUrl = "https://raw.githubusercontent.com/Jampamane/website/refs/heads/main/docs/assets/profile_pic.jpg";
+    const imageUrl = "https://raw.githubusercontent.com/Jampamane/website/refs/heads/main/docs/assets/background.jpg";
 
     const img = new Image()
     img.src = imageUrl;
